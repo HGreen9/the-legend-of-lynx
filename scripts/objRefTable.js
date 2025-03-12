6b623395af7391e4ad87fc2db57062fc86192853
Hannah Green <156165697+HGreen9@users.noreply.github.com>
2025-03-11 18:41:45 -0700

    Add files via upload

---
+++ scripts/objRefTable.js
@@ -0,0 +1,36 @@
+const C3 = self.C3;
+self.C3_GetObjectRefTable = function () {
+	return [
+		C3.Plugins.Text,
+		C3.Plugins.Mouse,
+		C3.Plugins.Flowchart,
+		C3.Plugins.Sprite,
+		C3.Plugins.System.Cnds.OnLayoutStart,
+		C3.Plugins.Flowchart.Acts.StartFlowchart,
+		C3.Plugins.Flowchart.Cnds.OnAnyNodeChange,
+		C3.Plugins.Sprite.Acts.SetAnimFrame,
+		C3.Plugins.Flowchart.Exps.OutputValue,
+		C3.Plugins.Text.Acts.SetText,
+		C3.Plugins.Mouse.Cnds.OnObjectClicked,
+		C3.Plugins.Flowchart.Acts.GotoNextFlowchartNode
+	];
+};
+self.C3_JsPropNameTable = [
+	{TextForScene: 0},
+	{PathB: 0},
+	{PathA: 0},
+	{Mouse: 0},
+	{FlowchartController: 0},
+	{Backgrounds: 0},
+	{textbg: 0}
+];
+
+self.InstanceType = {
+	TextForScene: class extends self.ITextInstance {},
+	PathB: class extends self.ITextInstance {},
+	PathA: class extends self.ITextInstance {},
+	Mouse: class extends self.IInstance {},
+	FlowchartController: class extends self.IInstance {},
+	Backgrounds: class extends self.ISpriteInstance {},
+	textbg: class extends self.ISpriteInstance {}
+}
